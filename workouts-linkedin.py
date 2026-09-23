aa4a46abd56a7c3ee6d3b44dabbe826bac3c2f01
Kevinsimon10 <kevinsimonleo10@gmail.com>
2026-09-24 01:29:38 +0530

    Add files via upload

---
+++ workouts-linkedin.py
@@ -0,0 +1,11 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print ('hello')
+hello
+>>> name='kevin'
+>>> print(name)
+kevin
+>>> print('A new begining in Python and Gen AI journey')
+A new begining in Python and Gen AI journey
+>>> print
+<built-in function print>
