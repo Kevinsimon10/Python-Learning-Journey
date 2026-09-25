f9d71ef5f3592bf67d87d410e9b881d60675fa65
Kevinsimon10 <kevinsimonleo10@gmail.com>
2026-09-25 14:58:52 +0530

    Add files via upload

---
+++ workouts-linkedin2.py
@@ -0,0 +1,39 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#variable name
+#variable names is case sensitive
+
+value=20
+Value=30
+print(Value)
+30
+
+#data types
+#int
+a=10
+#float
+>>> b=3.5
+>>> #string
+>>> c="leo"
+>>> #boolean
+>>> d=(a<b)
+>>> print(type(a))
+<class 'int'>
+>>> print(d)
+False
+>>> print(type(a))
+<class 'int'>
+>>> 
+>>> #string indexing
+>>> string="leo"
+>>> print(string[2])
+o
+>>> print(string[0])
+l
+>>> print(string[:2])
+le
+>>> string='stringbasics'
+>>> print(string[0:])
+stringbasics
+>>> print(string[3:7])
+ingb
